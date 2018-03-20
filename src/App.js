62f45b521950f8dbfac43bff77c0d1663626332d
Babak Javdan <Babakjav.done@gmail.com>
2018-03-20 01:27:46 -0400

    Checkpoint 2 first attempt

--- src/App.js
+++ src/App.js
@@ -1,19 +1,46 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import RoomList from './components/RoomList';
+import * as firebase from 'firebase';
+
+
+var config = {
+  apiKey: "AIzaSyCCJqbBeIflobtEAe1lfoexGMAjdnqoujM",
+  authDomain: "bloc-chat-fb34d.firebaseapp.com",
+  databaseURL: "https://bloc-chat-fb34d.firebaseio.com",
+  projectId: "bloc-chat-fb34d",
+  storageBucket: "bloc-chat-fb34d.appspot.com",
+  messagingSenderId: "87597374899"
+};
+
+firebase.initializeApp(config);
+
 
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state ={
+      activeRoom: ""
+    }
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Bloc Chat</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="flex-container">
+        <section className="App-rooms">
+          <RoomList
+            firebase={firebase}
+            activeRoom={this.state.activeRoom}
+          />
+        </section>
+        </div>
       </div>
+
     );
   }
 }
---
+++ src/components/RoomList.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+
+
+class RoomList extends Component{
+    constructor(props){
+      super(props);
+  
+      this.state ={
+        rooms:[],
+        newRoomName:""
+      };
+  
+      this.roomsRef = this.props.firebase.database().ref('rooms');
+  
+    }
+  
+  
+    componentDidMount() {
+         this.roomsRef.on('child_added', snapshot => {
+           const room = snapshot.val();
+           room.key = snapshot.key;
+           this.setState({ rooms: this.state.rooms.concat( room ) });
+  
+         });
+    }
+  
+    formUpdate(e) {
+      this.setState({ newRoomName:e.target.value })
+      console.log(this.state.newRoomName);
+    }
+  
+    createRoom() {
+        this.roomsRef.push({ name: this.state.newRoomName });
+      }
+  
+    setActiveRoom(){
+      this.setState({ activeRoom: this.state.rooms.name})
+    }
+    
+  
+    render(){
+      return (
+        <div>
+          <ul>
+           {this.state.rooms.map( (room, key) => (
+             <li key={key} onClick={() => this.setActiveRoom()}>{ room.name }</li>
+            )
+           )}
+          </ul>
+          <section>
+            <input type="text" onChange={ (e) => this.formUpdate(e) }></input>
+            <button type="submit" onClick={ () => this.createRoom() }>Submit</button>
+          </section>
+        </div>
+      )
+    };
+  }
+  
+  export default RoomList;
